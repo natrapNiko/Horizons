60544a6154211dd8818dc61ef1da1eed29e56dd0
natrapNiko <natrapniko@gmail.com>
2025-06-15 12:56:37 +0200

    Add Destination and Terrain validation constants
    Introduced two new static classes, `Destination` and `Terrain`, in the `ValidationConstatnts` class. The `Destination` class defines constants for name and description length limits, along with a date format string. The `Terrain` class specifies constants for name length limits.

--- Horizons.GCommon/ValidationConstatnts.cs
+++ Horizons.GCommon/ValidationConstatnts.cs
@@ -2,5 +2,21 @@
 {
     public static class ValidationConstatnts
     {
+        public static class Destination
+        {
+            public const int NameMaxLength = 80;
+            public const int NameMinLength = 3;
+
+            public const int DescriptionMaxLength = 250;
+            public const int DescriptionMinLength = 10;
+
+            public const string PublishedOnFormat = "dd-MM-yyyy";
+        }
+
+        public static class Terrain
+        {
+            public const int NameMaxLength = 20;
+            public const int NameMinLength = 3;
+        }
     }
 }
